0b7ebdd9511f413dece8781b70c13da5290de301
David <dj3pimpolhos@gmail.com>
2022-11-15 16:18:48 +0000

    First Setup

---
+++ src/main.java
@@ -0,0 +1,116 @@
+package src;
+
+import java.util.Scanner;
+
+public class main {
+    //-------------------------------------useful functions for all answers---------------------------------------------
+    static Scanner sc = new Scanner(System.in);
+
+    public static void printTempArray() {
+
+    }
+
+    public static void printMAArray(){
+
+    }
+
+    //convert temp array to MA array
+    public static void convertToMA(){
+
+    }
+
+    //
+
+    //calculates new temp array for a given temp deviation
+    public static void tempDeviation(){
+
+    }
+
+    //------------------------------------------------------A-----------------------------------------------------------
+    //reads location date and time
+    public static void readMetaData() {
+
+    }
+
+    //reads temperature array
+    public static void readArray() {
+
+    }
+
+    public static void A() {
+
+    }
+
+    //------------------------------------------------------B-----------------------------------------------------------
+    public static void B() {
+
+    }
+
+    //------------------------------------------------------C-----------------------------------------------------------
+    public static void C(){
+
+    }
+
+    //------------------------------------------------------D-----------------------------------------------------------
+    public static void D(){
+
+    }
+
+    //------------------------------------------------------E-----------------------------------------------------------
+    //calculates percentage of each alert type
+    public static void alertPercentage(){
+
+    }
+
+    public static void E(){
+
+    }
+
+    //------------------------------------------------------F-----------------------------------------------------------
+    //calculates temp rise to get catastrophic in all terrain
+    public static void tempRiseCatastrophic(){
+
+    }
+
+    public static void F(){
+
+    }
+
+    //------------------------------------------------------G-----------------------------------------------------------
+    //calculate the percentage of that changed
+    public static void alertChanged(){
+
+    }
+
+    public static void G(){
+
+    }
+
+    //------------------------------------------------------H-----------------------------------------------------------
+    public static void H(){
+
+    }
+
+    //------------------------------------------------------I-----------------------------------------------------------
+    public static void I(){
+
+    }
+
+    //------------------------------------------------------J-----------------------------------------------------------
+    public static void J(){
+        
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+
+    //handles answers, order and formatting
+    public static void answers() {
+
+    }
+
+
+    //initiates app
+    public static void main(String[] args) {
+
+    }
+}
